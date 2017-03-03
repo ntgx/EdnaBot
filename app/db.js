3f10003e863a3d0802b7e7eb82f988421c750630
nati <natig.88x@gmail.com>
2017-03-03 13:01:35 +0300

    fix for mongoose promise deprecation warning

--- app/db.js
+++ app/db.js
@@ -1,8 +1,9 @@
-const mongoose = require('mongoose');
-mongoose.connect(process.env.EDNA_MONGO_URI);
+const mongoose = require('mongoose')
+mongoose.Promise = global.Promise
+mongoose.connect(process.env.EDNA_MONGO_URI)
 
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
+var db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
 db.once('open', function() {
     console.log("we're connected!")
-});
+})
